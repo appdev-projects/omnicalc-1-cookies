514701c787c0428bba31979f4a83243a08839185
Jelani Woods <jelani@firstdraft.com>
2020-04-27 15:50:47 -0500

    Starting point

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+
+  get "/" => "application#square"
+
+  get "/square/new" => "application#square"
+  get "/square/results" => "application#square_results"
+  
+  get "/square_root/new" => "application#square_root"
+  get "/square_root/results" => "application#square_root_results"
+
+  get "/payment/new" => "application#payment"
+  get "/payment/results" => "application#payment_results"
+
+  get "/random/new" => "application#random"
+  get "/random/results" => "application#random_results"
+  
+  # Write your routes above this line
+  # =================================
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+end
